d212be286edd3dfc329cbe252c4a915b6a52b3ba
Solotole <solomontoleak47@gmail.com>
2024-07-24 09:12:09 +0100

    contains everything of the API routes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" views iporting blueprint """
+""" views iporting blueprint
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
