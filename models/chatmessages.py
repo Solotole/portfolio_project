1857ce0f4ba6283a44bfd40f9fbefc434553b04c
Solotole <solomontoleak47@gmail.com>
2024-07-24 08:11:38 +0100

    chat messages of users storage table

--- models/chatmessages.py
+++ models/chatmessages.py
@@ -2,11 +2,15 @@
 from sqlalchemy import Column, String, Text, ForeignKey
 from models.base_model import BaseModel, Base
 
-class ChatMessage(BaseModel, Base):
+class ChatMessage(BaseModel, Base): # user's chat messages storage class
     __tablename__ = 'chat_messages'
+
+    # user's unique id
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    # message sent by user
     message = Column(Text, nullable=False)
 
     def __init__(self, *args, **kwargs):
         """ Book class initialization special method """
+        # addressing BaseModel __init__magic method
         super().__init__(*args, **kwargs)
