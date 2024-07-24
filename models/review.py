c1c03144c885e4ac80f45052c40da836c809e9c9
Solotole <solomontoleak47@gmail.com>
2024-07-24 08:15:59 +0100

    users reviews and rating class-table wrapping

--- models/review.py
+++ models/review.py
@@ -5,18 +5,24 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel, Base):
+class Review(BaseModel, Base): # multiple inheritance
     """ Reviews class-table mapping """
     __tablename__ = "reviews"
 
+    # review comment
     text = Column(String(128), nullable=False)
+    # rating 0-10
     rating = Column(Integer, nullable=False)
+    # user's unique id
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     book_id = Column(String(60), ForeignKey('books.id'), nullable=False)
 
+    # relation to users table reviews
     user = relationship('User', back_populates='reviews')
+    # relation to books table reviews
     book = relationship('Book', back_populates='reviews')
 
     def __init__(self, *args, **kwargs):
         """ initialization method of class """
+         # addressing superclass initialization magic method
         super().__init__(*args, **kwargs)
