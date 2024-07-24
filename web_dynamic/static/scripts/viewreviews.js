054133e00c3ed9e21c967dc4066451aa369ec1ab
Solotole <solomontoleak47@gmail.com>
2024-07-24 11:48:47 +0100

    genereting reviews to a specific book

--- web_dynamic/static/scripts/viewreviews.js
+++ web_dynamic/static/scripts/viewreviews.js
@@ -1,40 +1,44 @@
 function generateReviewsHtml(reviews) {
-    return `
-        <li>
-            <div class="text-container">
-                <span class="reviewcomment">Review: ${reviews.text}</span>
-          <span class="reviewrating">Rate: ${reviews.rating}</span>
-          <ul class="options">
-                  <li class="delete" data-id="${reviews.id}">Delete</li>
-            <li class="update" data-id="${reviews.id}">Update</li>
-          </ul>
-            </div>
-        </li>
+  // genearates reviews of a specific book
+  return `
+    <li>
+      <div class="text-container">
+        <span class="reviewcomment">Review: ${reviews.text}</span>
+        <span class="reviewrating">Rate: ${reviews.rating}</span>
+        <ul class="options">
+          <li class="delete" data-id="${reviews.id}">Delete</li>
+          <li class="update" data-id="${reviews.id}">Update</li>
+        </ul>
+      </div>
+    </li>
     `;
   }
 $(document).ready(function () {
-    const urlreviews = "http://127.0.0.1:5001/api/v1/reviews/";
-    $('ul.books').on('click', '.view', function () {
-        $('header').html('Reviews');
-        $('.container').remove();
-        const bookId = $(this).data('id');
-        console.log('Book ID:', bookId);
-        $.ajax({
-          type: 'GET',
-          url: urlreviews + bookId,
-          contentType: 'application/json',
-          dataType: 'json',
-          success: (data) => {
-            console.log('Data received:', data);
-            $('ul.books').empty();
-            data.forEach((reviews) => {
-              const renderedReviewsHtml = generateReviewsHtml(reviews);
-              $('ul.books').append(renderedReviewsHtml);
-            });
-          },
-          error: (xhr, status, error) => {
-            console.error('Error fetching data:', status, error);
-          }
-        });
+  const urlreviews = "http://127.0.0.1:5001/api/v1/reviews/";
+  $('ul.books').on('click', '.view', function () {
+    $('header').html('Reviews');
+    // removing contentes of an element with class conatiner
+    $('.container').remove();
+    const bookId = $(this).data('id');
+    console.log('Book ID:', bookId);
+    $.ajax({
+      type: 'GET',
+      url: urlreviews + bookId,
+      contentType: 'application/json',
+      dataType: 'json',
+      success: (data) => {
+        console.log('Data received:', data);
+        $('ul.books').empty();
+        // looping over reviews data returned
+          data.forEach((reviews) => {
+            const renderedReviewsHtml = generateReviewsHtml(reviews);
+            // appending a block of all reviews by a user
+            $('ul.books').append(renderedReviewsHtml);
+          });
+      },
+      error: (xhr, status, error) => {
+        console.error('Error fetching data:', status, error);
+      }
     });
+  });
 });
