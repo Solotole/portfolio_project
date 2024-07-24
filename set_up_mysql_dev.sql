f1e89be305eed3ccc252e46772341cb2eac21dfd
Solotole <solomontoleak47@gmail.com>
2024-07-24 07:55:55 +0100

    setting up mysql database

--- set_up_mysql_dev.sql
+++ set_up_mysql_dev.sql
@@ -1,4 +1,4 @@
--- setting a mysql database
+- setting a mysql database
 CREATE DATABASE IF NOT EXISTS brrs_dev_db;
 CREATE USER IF NOT EXISTS `brrs_dev`@`localhost` IDENTIFIED BY 'Brrs_dev_pwd123!';
 GRANT ALL PRIVILEGES ON brrs_dev_db.* TO `brrs_dev`@`localhost`;
