7abf0461a4583a9dddad7e65dfde45d97082898e
Solotole <solomontoleak47@gmail.com>
2024-07-24 10:20:07 +0100

    rendering chat.html file

--- web_dynamic/chat.py
+++ web_dynamic/chat.py
@@ -4,7 +4,13 @@ from flask import Blueprint, render_template, session
 
 chat_bp = Blueprint('chat', __name__)
 
+
 @chat_bp.route('/chat/<user_id>')
 def chat(user_id):
-    return render_template('chat.html',
-                            user_id=user_id)
+    """ chat method rendering html
+        Args:
+            user_id: id belongng to user
+        Return:
+            return a rendered chat.html
+    """
+    return render_template('chat.html', user_id=user_id)
