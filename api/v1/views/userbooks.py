2b38906ba1f4e8fe81aff72c48867716d08034ee
Solotole <solomontoleak47@gmail.com>
2024-07-24 09:00:25 +0100

    user marked books

--- api/v1/views/userbooks.py
+++ api/v1/views/userbooks.py
@@ -8,9 +8,16 @@ from api.v1.views import app_views
 from models.booksread import UserBook
 
 
-@app_views.route('/users/<user_id>/read', methods=['POST'], strict_slashes=False)
+@app_views.route('/users/<user_id>/read', methods=['POST'],
+                 strict_slashes=False)
 def mark_as_read(user_id):
-    """ Mark a book as read """
+    """ Mark a book as read
+        Args:
+            user_id: user id
+        Return:
+            return a list of deserializes book id
+            with status code 201
+    """
     book_id = request.json.get('book_id')
     if not book_id:
         return abort(400, description="book_id is required")
@@ -21,8 +28,14 @@ def mark_as_read(user_id):
     return make_response(jsonify(read_entry.to_dict()), 201)
 
 
-@app_views.route('/users/<user_id>/read', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<user_id>/read', methods=['GET'],
+                 strict_slashes=False)
 def get_read_books(user_id):
-    """ Get books a user has read """
+    """ Get books a user has read
+        Args:
+            user_id: user id
+        Return:
+            returns list of all marked books by a user
+    """
     read_books = storage.get_read_books(user_id)
     return jsonify([book.to_dict() for book in read_books])
