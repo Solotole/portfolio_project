b5907c3c07e726578f87cc9d1e1ebd6edc925804
Solotole <solomontoleak47@gmail.com>
2024-07-24 09:34:44 +0100

    users details db storage

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
- # multiple inheritance definition
+# multiple inheritance definition
 class User(BaseModel, Base):
     """ User class-table mapping representation """
     __tablename__ = "users"
@@ -13,11 +13,12 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False) # hashed using sha1 method
+    password = Column(String(128), nullable=False)  # hashed using sha1 method
 
     reviews = relationship('Review', back_populates='user',
                            cascade="all, delete, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         """ initialization method of class User"""
-        super().__init__(*args, **kwargs) # addressing siper user __init__ method
+        # addressing siper user __init__ method
+        super().__init__(*args, **kwargs)
