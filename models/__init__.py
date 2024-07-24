2589b8457385b637e3a9962f1dacd50c80699ec4
Solotole <solomontoleak47@gmail.com>
2024-07-24 07:58:33 +0100

    init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """ models package """
-from os import getenv
 from models.engine.db_storage import DBStorage
 
-
-if getenv('BRRS_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-    storage.reload()
+storage = DBStorage()
+storage.reload()
