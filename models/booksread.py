96098d6631c253af353fd0739dd49655c3e711a9
Solotole <solomontoleak47@gmail.com>
2024-07-24 09:29:13 +0100

    class-table containing users marked books

--- models/booksread.py
+++ models/booksread.py
@@ -5,13 +5,15 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
 
 
-class UserBook(BaseModel, Base): # multiple inheritance of the base class and Base
+# multiple inheritance of the base class and Base
+class UserBook(BaseModel, Base):
     """ Class-table to represent books read by the user """
     __tablename__ = 'userbooks'
 
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     book_id = Column(String(60), ForeignKey('books.id'), nullable=False)
-    
+
     def __init__(self, *args, **kwargs):
         """ UserBook class initialization special method """
-        super().__init__(*args, **kwargs) # parsing to Base class __init__ magic method
+        # parsing to Base class __init__ magic method
+        super().__init__(*args, **kwargs)
