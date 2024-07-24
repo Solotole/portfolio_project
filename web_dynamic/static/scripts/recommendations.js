00e7adebfd54eb8d8d3f3b503567ecaaa9bc86d4
Solotole <solomontoleak47@gmail.com>
2024-07-24 11:09:02 +0100

    addressing book recommendation by user

--- web_dynamic/static/scripts/recommendations.js
+++ web_dynamic/static/scripts/recommendations.js
@@ -1,18 +1,20 @@
 function generateBookRecHtml(books) {
+  // returns a block of recommedated books data
     return `
         <li>
-        <div class="text-container">
-          <span>Title: ${books.name}</span>
-                <span>Author: ${books.author}</span>
-                <span>Genre: ${books.genre}</span>
-          <ul class="options">
-            <li class="view" data-id="${books.id}">view</li>
-          </ul>
-            </div>
+          <div class="text-container">
+            <span>Title: ${books.name}</span>
+            <span>Author: ${books.author}</span>
+            <span>Genre: ${books.genre}</span>
+            <ul class="options">
+              <li class="view" data-id="${books.id}">view</li>
+            </ul>
+          </div>
         </li>
     `;
   };
   $(document).ready(function () {
+    // accessing user's id
     const userId = $('ul.books').data('user-id');
     $.ajax({
         type: 'GET',
@@ -21,11 +23,13 @@ function generateBookRecHtml(books) {
         dataType: 'json',
         success: (data) => {
           console.log('Data received:', data);
+          // looping over each data returned by API
           data.forEach((books) => {
             const renderedHtml = generateBookRecHtml(books);
             $('ul.books').append(renderedHtml);
           });
         },
+        // error addressing
         error: (xhr, status, error) => {
           console.error('Error fetching data:', status, error);
         }
