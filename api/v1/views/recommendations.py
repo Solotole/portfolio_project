d16cc26a6fc9db9e0d564adc7c243b13052b2dd0
Solotole <solomontoleak47@gmail.com>
2024-07-24 08:56:26 +0100

    recommendations API route

--- api/v1/views/recommendations.py
+++ api/v1/views/recommendations.py
@@ -7,21 +7,25 @@ from models.book import Book
 from models.user import User
 from models.review import Review
 
-@app_views.route('/recommendations/<id_user>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/recommendations/<id_user>', methods=['GET'],
+                 strict_slashes=False)
 def recommend(id_user):
     """ route-method to recommend for books according to
         ratings, genre and author's name
+        Args:
+            id_user: user's id
+        Return:
+            returns a list of serialized recommeded books
     """
-        # user data object
+    recommended_books_serialized = []
+    # user data object
     user = storage.get(User, id_user)
-
-    # Check if the user exists
+    # Checking if the user exists
     if user is None:
         return jsonify({'error': 'User not found'}), 404
-
     # Retrieving user's reviews with a rating above 5
     user_reviews = storage.user_reviews(id_user)
-
     # Collect authors and genres from highly-rated books
     reviewed_authors = set()
     reviewed_genres = set()
@@ -31,17 +35,13 @@ def recommend(id_user):
         reviewed_authors.add(review.book.author)
         reviewed_genres.add(review.book.genre)
         books_reviewed_ids.add(review.book.id)
-
     # Retrieve all books
     all_books = storage.all(Book).values()
-
-    # Generate recommendations
+    # Generating recommendations
     recommended_books = []
     for book in all_books:
         if book.id not in books_reviewed_ids and (book.author in reviewed_authors or book.genre in reviewed_genres):
             recommended_books.append(book)
-
-    # Convert the recommended books to a serializable format
+    # Converting the recommended books to a serializable format
     recommended_books_serialized = [{'name': book.name, 'author': book.author, 'genre': book.genre} for book in recommended_books]
-
     return make_response(jsonify(recommended_books_serialized), 200)
